c7c48ed8dd248ed069902801db1d64fcd541759c
XiezibanWrite <103623519+XiezibanWrite@users.noreply.github.com>
2023-04-09 08:11:05 +0800

    增加多语言系统 | 更改退出为按下任意键而不是Enter（技术稍微升级了点）

--- 车队姬模拟/Program.cs
+++ 车队姬模拟/Program.cs
@@ -13,34 +13,76 @@ namespace 车队姬模拟
         private static byte[] data = new byte[1024];
         static void Main(string[] args)
         {
-            Console.Write("免责声明：此程序仅供娱乐 如被车队姬封禁 本人概不负责 如接受此免责条款 请输入1 否则请退出此程序：\n");
-            int mianzetiaokuan = Convert.ToInt32(Console.ReadLine());
-            if (mianzetiaokuan == 1)
+            Console.WriteLine("请选择语言/Please Choose your Language\n\nEnglish - Input “English”\n简体中文 - 请输入 “简体中文”");
+            string language = Console.ReadLine();
+            switch (language)
             {
-                Console.Write("模拟的房间代码：");
-                string a = Console.ReadLine();
-                Console.Write("模拟的模组版本：");
-                string b = Console.ReadLine();
-                Console.Write("模拟的人数：");
-                string c = Console.ReadLine();
+                case "English":
+                    Console.Write("Disclaimer: This program is for entertainment only. If banned by the program, I am not responsible. To accept this disclaimer, please enter 'I agree to this disclaimer'. Otherwise, please exit this program:\n");
+                    string mianzetiaokuanEN = Console.ReadLine();
+                    if (mianzetiaokuanEN == "I agree to this disclaimer")
+                    {
+                        Console.Write("Simulated room code：");
+                        string a = Console.ReadLine();
+                        Console.Write("Simulated mod version：");
+                        string b = Console.ReadLine();
+                        Console.Write("Number of simulated people：");
+                        string c = Console.ReadLine();
 
-                string msg = a + "|" + b + "|" + c;
-                byte[] buffer = new byte[2048];
-                buffer = Encoding.Default.GetBytes(msg);
+                        string msg = a + "|" + b + "|" + c;
+                        byte[] buffer = new byte[2048];
+                        buffer = Encoding.Default.GetBytes(msg);
 
-                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                clientSocket.Connect(IP, PORT);
-                //clientSocket.BeginReceive(data, 0, data.Length, SocketFlags.None, CallBack, null);
-                clientSocket.Send(buffer);
-                clientSocket.Close();
-                Console.WriteLine("\n执行完毕 请按Enter键退出");
-                Console.ReadLine();
+                        clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                        clientSocket.Connect(IP, PORT);
+                        //clientSocket.BeginReceive(data, 0, data.Length, SocketFlags.None, CallBack, null);
+                        clientSocket.Send(buffer);
+                        clientSocket.Close();
+                        Console.WriteLine("\nAfter execution, please press any key to exit");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.Write("Input error, please press any key to exit");
+                        Console.ReadKey();
+                    }
+                    break;
+                case "简体中文":
+                    Console.Write("免责声明：此程序仅供娱乐 如被车队姬封禁 本人概不负责 如接受此免责条款 请输入 “ 我同意本免责条款” 否则请退出此程序：\n");
+                    string mianzetiaokuan = Console.ReadLine();
+                    if (mianzetiaokuan == "我同意本免责条款")
+                    {
+                        Console.Write("模拟的房间代码：");
+                        string a = Console.ReadLine();
+                        Console.Write("模拟的模组版本：");
+                        string b = Console.ReadLine();
+                        Console.Write("模拟的人数：");
+                        string c = Console.ReadLine();
+
+                        string msg = a + "|" + b + "|" + c;
+                        byte[] buffer = new byte[2048];
+                        buffer = Encoding.Default.GetBytes(msg);
+
+                        clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                        clientSocket.Connect(IP, PORT);
+                        //clientSocket.BeginReceive(data, 0, data.Length, SocketFlags.None, CallBack, null);
+                        clientSocket.Send(buffer);
+                        clientSocket.Close();
+                        Console.WriteLine("\n执行完毕 请按任意键退出");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.Write("输入错误 请按任意键退出");
+                        Console.ReadKey();
+                    }
+                    break;
+                    default:
+                    Console.Write("Input wrong Language Code, please press any key to exit / 输入了错误的语言代码 请按任意键退出");
+                    Console.ReadKey();
+                    break;
             }
-            else
-            {
-                Console.Write("输入错误 请按Enter键退出");
-                Console.ReadLine();
-            }
+            
             
 
 
--- 车队姬模拟/obj/Release/netcoreapp3.1/车队姬模拟.AssemblyInfo.cs
+++ 车队姬模拟/obj/Release/netcoreapp3.1/车队姬模拟.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("车队姬模拟")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("车队姬模拟")]
